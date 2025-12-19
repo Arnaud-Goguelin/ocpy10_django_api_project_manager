e533cf83d34ffc290206495acd506c23ea3ba9e3
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-12-19 10:17:34 +0100

    feat(aut hviews): overwrite rest_framework_simplejwt.views to create docs for swagger

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .views import LogoutView, DecoratedTokenRefreshView, DecoratedTokenObtainPairView
 
 app_name = "auth"
 
 urlpatterns = [
-    path("", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/", DecoratedTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", DecoratedTokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,19 +1,64 @@
 import logging
 
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 logger = logging.getLogger(__name__)
 
 
+@extend_schema_view(
+    post=extend_schema(
+        summary="Obtain Obtain JWT token pair",
+        description="Takes a set of user credentials and returns an access and refresh JSON web token "
+                    "pair. "
+                    "<br>Returns a `200` response code on success. "
+                    "<br>Raises a `400` error code if the request body is not valid. "
+                    "<br>Raises a `401` error code if the user credentials are invalid."
+                    "<br>"
+                    "<br>**Authentification required**: No"
+                    "<br>**Permissions required**: None ",
+        tags=["Auth"]
+    )
+)
+class DecoratedTokenObtainPairView(TokenObtainPairView):
+    pass
+
+
+@extend_schema_view(
+    post=extend_schema(
+        summary="Refresh JWT token",
+        description="Takes a refresh type JSON web token and returns an access type JSON web token if the refresh "
+                    "token is valid. "
+                    "<br>Returns a `200` response code on success."
+                    "<br>Raises a `400` error code if the request body is not valid."
+                    "<br>Raises a `401` error code if the refresh token is invalid."
+                    "<br>"
+                    "<br>**Authentification required**: No"
+                    "<br>**Permissions required**: None",
+        tags=["Auth"]
+    )
+)
+class DecoratedTokenRefreshView(TokenRefreshView):
+    pass
+
+
+@extend_schema_view(
+    post=extend_schema(
+        summary="Logout user and invalidate refresh token",
+        tags=["Auth"])
+)
 class LogoutView(APIView):
     """
-    User logout endpoint - blacklists the refresh token.
-    POST /api/auth/logout/
-    Body: {"refresh": "..."}
+    Represents the user view for logout.
+    <br>Returns a `200` response code on success.
+    <br>Raises a `400` error code if no refresh token is provided in the request body.
+    <br>
+    <br>**Authentification required**: Yes
+    <br>**Permissions required**: `IsAuthenticated`,
     """
 
     permission_classes = [IsAuthenticated]
