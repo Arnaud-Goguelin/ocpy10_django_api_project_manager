7a3a054ffde25782afb7c44ec540a52933c9c189
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-12-19 13:47:47 +0100

    feat(project serializerÃ): transfer ContributorSerializer from user app to project app

--- project/serializers.py
+++ project/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import IntegerField, ModelSerializer, Serializer
 
 from .models import Project
 
@@ -7,3 +7,9 @@ class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
         fields = ["name", "type", "description", "author", "contributors", "created_at"]
+
+
+class ContributorSerializer(Serializer):
+    """Serializer for adding a contributor to a project"""
+
+    user_id = IntegerField(required=True, help_text="ID of the user to add as contributor")
--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import IntegerField, ModelSerializer, Serializer
+from rest_framework.serializers import ModelSerializer
 
 from .models import User
 
@@ -10,9 +10,3 @@ class UserSerializer(ModelSerializer):
             "username",
             "email",
         ]
-
-
-class ContributorSerializer(Serializer):
-    """Serializer for adding a contributor to a project"""
-
-    user_id = IntegerField(required=True, help_text="ID of the user to add as contributor")
