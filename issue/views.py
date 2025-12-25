24e5f52732bd1fb3f92e5f60cab88aaab5290578
Arnaud <124574198+Arnaud-Goguelin@users.noreply.github.com>
2025-12-25 19:13:22 +0100

    Fix/issues and comments endpoints (#9)

--- issue/views.py
+++ issue/views.py
@@ -36,6 +36,16 @@ class IssueModelViewSet(ModelViewSet):
     serializer_class = IssueSerializer
     permission_classes = []
 
+    def get_queryset(self):
+        """Filter issues by project_id from URL"""
+        project_id = self.kwargs.get("project_id")
+        return Issue.objects.filter(project_id=project_id)
+
+    def perform_create(self, serializer):
+        """Automatically set author and project when creating an issue"""
+        project_id = self.kwargs.get("project_id")
+        serializer.save(author=self.request.user, project_id=project_id)
+
 
 @extend_schema_view(
     list=extend_schema(
@@ -67,3 +77,13 @@ class CommentModelViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = []
+
+    def get_queryset(self):
+        """Filter issues by issue_id from URL"""
+        issue_id = self.kwargs.get("issue_id")
+        return Comment.objects.filter(issue_id=issue_id)
+
+    def perform_create(self, serializer):
+        """Automatically set author and issue when creating a comment"""
+        issue_id = self.kwargs.get("issue_id")
+        serializer.save(author=self.request.user, issue_id=issue_id)
