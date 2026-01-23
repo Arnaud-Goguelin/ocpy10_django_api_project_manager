bd301d82844c0b2d45084fcdc519d1eeb06d8536
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-01-23 18:45:49 +0100

    fix(ProjectMixin): call super constructor at last to allow validation with project object already attached to view

--- config/mixins.py
+++ config/mixins.py
@@ -16,8 +16,6 @@ class ProjectMixin:
         Set project from URL as an instance attribute.
         Called before every action (list, create, retrieve, etc.)
         """
-        super().initial(request, *args, **kwargs)
-
         # Get and validate project_id from URL
         project_id = self.kwargs.get("project_id")
 
@@ -26,3 +24,6 @@ class ProjectMixin:
                 self.project = Project.objects.get(id=project_id)
             except Project.DoesNotExist as error:
                 raise NotFound(f"Project with id {project_id} does not exist.") from error
+
+        # once self.project is set, call super to proceed with view initialization and permissions
+        super().initial(request, *args, **kwargs)
